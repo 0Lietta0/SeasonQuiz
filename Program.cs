73cc708d1e33f58b3b399f00c9a36f9bf58285a7
Lietta <kamila0mirek@gmail.com>
2023-08-31 11:04:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+//-welcome user
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("\tQ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("U");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("I");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Z");
+Console.ResetColor();
+
+Console.WriteLine("\nWhich season are you?");
+Console.Write("\nWrite your username: ");
+string? readResult = Console.ReadLine();
+Console.WriteLine($"\nWelcome {readResult}!");
+Console.WriteLine("\nPress enten to start.");
+
+
+//-ask 5 questions(a, b, c, d)
+
+
+
+//- display result
