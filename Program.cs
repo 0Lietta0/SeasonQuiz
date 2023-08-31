3d0b643e41b75a769ae3b0eb8c270718546b5c64
Lietta <kamila0mirek@gmail.com>
2023-08-31 11:58:18 +0200

    implement RightAnswer method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+string? readResult;
+
 //-welcome user
 Console.ForegroundColor = ConsoleColor.Green;
 Console.Write("\tQ");
@@ -11,23 +13,56 @@ Console.WriteLine("Z");
 Console.ResetColor();
 Console.WriteLine("\nWhich season are you?");
 
-//-get username
-string userName = "";
-do
-{
-    Console.Write("\nWrite your username: ");
-    string? readResult = Console.ReadLine();
-    if (readResult != null)
-        userName = readResult.Trim();
-} while (userName == "");
+////-get username
+//string userName = "";
+//do
+//{
+//    Console.Write("\nWrite your username: ");
+//    readResult = Console.ReadLine();
+//    if (readResult != null)
+//        userName = readResult.Trim();
+//} while (userName == "");
 
-Console.WriteLine($"\nWelcome {userName}!");
-Console.WriteLine("\nPress enten to start.");
+//Console.WriteLine($"\nWelcome {userName}!");
+//Console.WriteLine("\nPress enten to start.");
 
-Console.ReadLine();
+//Console.ReadLine();
+//Console.Clear();
 
 //-ask 5 questions(a, b, c, d)
+
+Console.WriteLine("Which color do you like the most?.");
+WriteAnswers("green", "yellow", "red", "blue\n");
+do
+{
+    RightAnswers();
+
+} while (RightAnswers() == false);
 
 
 
 //- display result
+
+void WriteAnswers(string a, string b, string c, string d)
+{
+    Console.WriteLine("a) {0}\tb) {1}\tc) {2}\td) {3}", a, b, c, d);
+}
+
+bool RightAnswers()
+{
+    string userAnswer = "";
+
+    readResult = Console.ReadLine();
+    if (readResult != null)
+        userAnswer = readResult.Trim().ToLower();
+
+    if (userAnswer == "a" || userAnswer == "b" || userAnswer == "c" || userAnswer == "d")
+    {
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("\nSorry, you're answer is incorrect. Write: a, b, c or d to continue.\n");
+        return false;
+    }
+}
