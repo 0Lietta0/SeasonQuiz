a5bdb70f189482b9a941085f9939421e3150d108
Lietta <kamila0mirek@gmail.com>
2023-08-31 11:15:36 +0200

    get username

--- Program.cs
+++ Program.cs
@@ -9,13 +9,22 @@ Console.Write("I");
 Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine("Z");
 Console.ResetColor();
-
 Console.WriteLine("\nWhich season are you?");
-Console.Write("\nWrite your username: ");
-string? readResult = Console.ReadLine();
-Console.WriteLine($"\nWelcome {readResult}!");
+
+//-get username
+string userName = "";
+do
+{
+    Console.Write("\nWrite your username: ");
+    string? readResult = Console.ReadLine();
+    if (readResult != null)
+        userName = readResult.Trim();
+} while (userName == "");
+
+Console.WriteLine($"\nWelcome {userName}!");
 Console.WriteLine("\nPress enten to start.");
 
+Console.ReadLine();
 
 //-ask 5 questions(a, b, c, d)
 
